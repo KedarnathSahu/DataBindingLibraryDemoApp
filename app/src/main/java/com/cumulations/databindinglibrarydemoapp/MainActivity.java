ec482ceb845eb86fec953b936c099495e9093b42
kedarnath <kedarnath.sahu97@gmail.com>
2019-01-24 16:06:39 +0530

    click listeners

--- app/src/main/java/com/cumulations/databindinglibrarydemoapp/MainActivity.java
+++ app/src/main/java/com/cumulations/databindinglibrarydemoapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.cumulations.databindinglibrarydemoapp;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableField;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.cumulations.databindinglibrarydemoapp.databinding.ActivityMainBinding;
 
@@ -11,8 +17,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        User user = new User("Rahul", "Jha");
-        binding.setUser(user);
+        binding.setUser(new User("Rahul", "Jha"));
+
+        binding.setClickHandler(new ClickHandler(this));
     }
+
+    public class ClickHandler {
+        private Context context;
+
+        public ClickHandler(Context context) {
+            this.context = context;
+        }
+
+        public void onClickOk(View view) {
+             Toast.makeText(context, "simpleButtonClick", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
