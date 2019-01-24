a65623f7d3ef6f1ede223b8036bce465dcf84491
kedarnath <kedarnath.sahu97@gmail.com>
2019-01-24 18:03:22 +0530

    Update UI using Observable objects

--- app/src/main/java/com/cumulations/databindinglibrarydemoapp/MainActivity.java
+++ app/src/main/java/com/cumulations/databindinglibrarydemoapp/MainActivity.java
@@ -2,32 +2,33 @@ package com.cumulations.databindinglibrarydemoapp;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
-import android.databinding.ObservableField;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.cumulations.databindinglibrarydemoapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setUser(new User("Rahul", "Jha"));
 
-        binding.setClickHandler(new ClickHandler(this));
+        binding.setClickHandler1(new ClickHandler1(this));
+        binding.setClickHandler2(new ClickHandler2(this, binding.content2.firstNameIncludedLayout2, binding.content2.lastNameIncludedLayout2));
     }
 
-    public class ClickHandler {
+    public class ClickHandler1 {
         private Context context;
 
-        public ClickHandler(Context context) {
+        public ClickHandler1(Context context) {
             this.context = context;
         }
 
@@ -42,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
 
         public void onClickWithParameters(View view, User user) {
             Toast.makeText(context, user.firstName + " " + user.lastName, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class ClickHandler2 {
+        private Context context;
+        private EditText fName, lName;
+
+        public ClickHandler2(Context context, EditText fName, EditText lName) {
+            this.context = context;
+            this.fName = fName;
+            this.lName = lName;
+        }
+
+        public void onUpdateUserInfo(View view) {
+            binding.setUser(new User(fName.getText().toString(), lName.getText().toString()));
+            Toast.makeText(context, "onUpdateUserInfo", Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/cumulations/databindinglibrarydemoapp/User.java
+++ app/src/main/java/com/cumulations/databindinglibrarydemoapp/User.java
@@ -1,10 +1,26 @@
 package com.cumulations.databindinglibrarydemoapp;
 
-public class User {
-    public final String firstName;
-    public final String lastName;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class User extends BaseObservable {
+    public String firstName;
+    public String lastName;
+
     public User(String firstName, String lastName) {
         this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
         this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
+    }
+
+    @Bindable
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Bindable
+    public String getLastName() {
+        return lastName;
     }
 }
